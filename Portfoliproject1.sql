645a7b034621882cdf8bc1b1b809cd017fadb92a
Rakesh-Kashyap-git <83485685+Rakesh-Kashyap-git@users.noreply.github.com>
2022-03-20 14:26:59 +0530

    Add files via upload
    Third Commit

--- Portfoliproject1.sql
+++ Portfoliproject1.sql
@@ -2,10 +2,6 @@ SELECT *
 FROM PortfoliProject.dbo.CovidDeaths$
 
 
---SELECT * 
---FROM PortfoliProject.dbo.CovidVaccinations$
---order by 3,4
-
 SELECT location,date,total_cases,new_cases,total_deaths,population 
 FROM PortfoliProject.dbo.CovidDeaths$ 
 order by 1,2
@@ -152,7 +148,7 @@ new_deaths = (select min(new_deaths) from PortfoliProject.dbo.CovidDeaths$ where
 and location = 'India' order by 1,2
 
 
--- Continent numbers
+-- CONTINENT NUMBERS
 
 -- Continents with Highest Infection  
 
@@ -192,12 +188,12 @@ order by Maximum_cases DESC
 
 
 
---Vaccination and Test Numbers
+--VACCINATION AND TEST NUMBERS 
 
 
 
 
--- Global Numbers
+-- GLOBAL NUMBERS
 
 
 select * from PortfoliProject.[dbo].[CovidVaccinations$] order by date
@@ -348,20 +344,123 @@ From TempPopVac
 
 use PortfoliProject
 
-CREATE VIEW PercentPeopleVaccinated as 
-Select death.continent, death.location, death.date, death.population, vacc.new_vaccinations
-, 
-CASE CAST(new_vaccinations AS float)
-     WHEN NULL then NULL
-     ELSE SUM(CONVERT(float,vacc.new_vaccinations)) OVER (Partition by death.Location Order by death.location, death.Date) 
-END as RollingPeopleVaccinated
+--CREATE VIEW PercentPeopleVaccinated as 
+--Select death.continent, death.location, death.date, death.population, vacc.new_vaccinations
+--, 
+--CASE CAST(new_vaccinations AS float)
+--     WHEN NULL then NULL
+--     ELSE SUM(CONVERT(float,vacc.new_vaccinations)) OVER (Partition by death.Location Order by death.location, death.Date) 
+--END as RollingPeopleVaccinated
+--From PortfoliProject.dbo.CovidDeaths$ death join PortfoliProject.[dbo].[CovidVaccinations$] vacc
+--on
+--death.location = vacc.location and death.date = vacc.date
+--where death.continent is not null and new_vaccinations is not null
+
+--select * from PercentPeopleVaccinated
+
+
+-- INDIAN NUMBERS
+
+
+select * from PortfoliProject.[dbo].[CovidVaccinations$] where location = 'India' order by date 
+
+
+select count(*) from PortfoliProject.[dbo].[CovidVaccinations$] where location = 'India'  
+
+
+
+-- Total Tests done in India
+select SUM(cast(new_tests as float)) as Total_Cases_ from PortfoliProject.[dbo].[CovidVaccinations$] where location = 'India'
+
+
+-- Max. Positive rate in a day in India
+select date, location,new_tests,continent, positive_rate from PortfoliProject.[dbo].[CovidVaccinations$]
+where location = 'India' and positive_rate = 
+( 
+select MAX(cast(positive_rate as float)) from PortfoliProject.[dbo].[CovidVaccinations$] 
+where cast(positive_rate as float)!=0 and positive_rate is not null and location = 'India'
+)
+
+--Min Positive rate in a day in India
+select date, location,new_tests,continent, positive_rate from PortfoliProject.[dbo].[CovidVaccinations$]
+where location = 'India' and positive_rate = 
+( 
+select MIN(cast(positive_rate as float)) from PortfoliProject.[dbo].[CovidVaccinations$] 
+where cast(positive_rate as float)!=0 and positive_rate is not null and location = 'India'
+)
+
+
+-- Max. Vaccination in a day in India
+select date, location,new_tests,continent, new_vaccinations,positive_rate from PortfoliProject.[dbo].[CovidVaccinations$]
+where location = 'India' and new_vaccinations = 
+( 
+select MAX(cast(new_vaccinations as float)) from PortfoliProject.[dbo].[CovidVaccinations$] 
+where cast(new_vaccinations as float)!=0 and new_vaccinations is not null and  continent is not null and 
+location = 'India'
+)
+
+--Min Vaccinations in a day in India 
+select date, location,new_tests,continent,new_vaccinations, positive_rate from PortfoliProject.[dbo].[CovidVaccinations$]
+where location = 'India' and new_vaccinations = 
+( 
+select MIN(cast(new_vaccinations as float)) from PortfoliProject.[dbo].[CovidVaccinations$] 
+where cast(new_vaccinations as float)!=0 and new_vaccinations is not null and continent is not null and 
+location = 'India'
+)
+
+
+
+
+-- Vaccination Vs Population
+select max(vacc.location) as Location, sum(cast(vacc.new_vaccinations as float)) as Total_Vaccinations ,max(death.population) as population_,
+( sum(cast(vacc.new_vaccinations as float))/max(death.population) )*100 as Vaccination_percentage
+from  
+PortfoliProject.dbo.CovidDeaths$ death join PortfoliProject.[dbo].[CovidVaccinations$] vacc
+on
+death.location = vacc.location and death.date = vacc.date
+where vacc.continent is not null 
+and vacc.location = 'India'
+order by Vaccination_percentage desc
+
+
+-- Death Count V/S life_expectancy
+
+Select MAX(vacc.Location) as location , MAX(death.population) as population ,MAX(death.Total_deaths) as TotalDeaths , 
+((MAX(death.Total_deaths)/MAX(death.population))*100) as PercentPopulationDead , MAX(vacc.life_expectancy) as life_expectancy_
 From PortfoliProject.dbo.CovidDeaths$ death join PortfoliProject.[dbo].[CovidVaccinations$] vacc
 on
 death.location = vacc.location and death.date = vacc.date
-where death.continent is not null and new_vaccinations is not null
+Where vacc.continent is not null 
+and death.location = 'India'
+order by PercentPopulationDead desc
 
-select * from PercentPeopleVaccinated
 
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+With TempPopVacInd (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as(
+Select death.continent, death.location, death.date, death.population, vacc.new_vaccinations,
+SUM(CONVERT(float,vacc.new_vaccinations)) OVER (order by death.date) as RollingPeopleVaccinated
+From PortfoliProject.dbo.CovidDeaths$ death join PortfoliProject.[dbo].[CovidVaccinations$] vacc
+on
+death.location = vacc.location and death.date = vacc.date
+where vacc.location = 'India' and death.continent is not null and new_vaccinations is not null
+)
+select *, round((RollingPeopleVaccinated/Population)*100,2) as Rollingpercent from TempPopVacInd
+
+
+use PortfoliProject
+
+
+CREATE VIEW PercentPeopleVaccinatedInd as 
+Select death.continent, death.location, death.date, death.population, vacc.new_vaccinations,
+SUM(CONVERT(float,vacc.new_vaccinations)) OVER (order by death.date) as RollingPeopleVaccinated
+From PortfoliProject.dbo.CovidDeaths$ death join PortfoliProject.[dbo].[CovidVaccinations$] vacc
+on
+death.location = vacc.location and death.date = vacc.date
+where vacc.location = 'India' and death.continent is not null and new_vaccinations is not null
+
+select * from PercentPeopleVaccinatedInd
 
 
 
