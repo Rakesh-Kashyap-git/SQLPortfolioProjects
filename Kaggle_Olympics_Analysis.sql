0e101f156f7eade5fbd56f7dfb83d09e81632f6b
Rakesh Kashyap <83485685+Rakesh-Kashyap-git@users.noreply.github.com>
2022-06-25 07:24:13 +0530

    Add files via upload

---
+++ Kaggle_Olympics_Analysis.sql
@@ -0,0 +1,31 @@
+use PortfolioProject;
+
+select * from PortfolioProject.dbo.olympics_history;
+
+-- 1. How many olympics games have been held?
+
+    select count(distinct games) as total_olympic_games
+    from olympics_history;
+--  Ans: 51
+
+
+-- 2. List down all Olympics games held so far.
+
+    select distinct year,season,city
+    from olympics_history
+    order by year;
+
+
+-- 3. Mention the total no of nations who participated in each olympics game?
+
+    with all_countries as
+        (select games, nr.region
+        from olympics_history oh
+        join olympics_history_noc_regions nr ON nr.noc = oh.noc
+        group by games, nr.region)
+    select games, count(games) as total_countries
+    from all_countries
+    group by games
+    order by total_countries;
+
+	 
